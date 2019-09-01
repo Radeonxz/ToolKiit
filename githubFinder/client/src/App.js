bd8e7b2f55c72eb38e91908c820157d39d197b38
Readonxz <xuanxxzhao@gmail.com>
2019-08-31 20:41:08 -0400

    add navbar

--- githubFinder/client/src/App.js
+++ githubFinder/client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1></h1>
+      <div className='App'>
+        <Navbar
+          title='Github Finder'
+          icon='fab fa-github'
+        />
       </div>
     );
   }
