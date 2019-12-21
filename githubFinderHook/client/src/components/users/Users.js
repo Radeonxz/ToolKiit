9795227429998479bb9c3020558237bb8112bc2a
Radeonxz <xuanxxzhao@gmail.com>
2019-12-21 17:23:19 -0500

    chore: refactor app and users

--- githubFinderHook/client/src/components/users/Users.js
+++ githubFinderHook/client/src/components/users/Users.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import UserItem from './UserItem';
-import Spinner from '../layout/Spinner/Spinner';
+import React from "react";
+import PropTypes from "prop-types";
+import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner/Spinner";
 
 const Users = ({ users, loading }) => {
+  console.log("users is", users);
+  console.log("loading is", loading);
   return loading ? (
-      <Spinner />
+    <Spinner />
   ) : (
     <div style={userStyle}>
       {users.map(user => (
         <UserItem key={user.id} user={user} />
       ))}
     </div>
-  )
-}
+  );
+};
 
 UserItem.propTypes = {
   users: PropTypes.array.isRequired,
   loading: PropTypes.bool.isRequired
-}
+};
 
 const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem'
-}
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem"
+};
 
 export default Users;
