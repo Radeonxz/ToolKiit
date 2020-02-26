c89a43840127c9a38f643ec3bf1970c361569604
Radeonxz <xuanxxzhao@gmail.com>
2020-02-25 22:28:18 -0500

    chore: add notfound page

--- githubFinderHook/client/src/App.js
+++ githubFinderHook/client/src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert/Alert";
+import User from "./components/users/User";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
-import User from "./components/users/User";
+import NotFound from "./components/pages/NotFound";
 
 // import context and state
 import GithubState from "./context/github/GithubState";
@@ -25,6 +26,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
