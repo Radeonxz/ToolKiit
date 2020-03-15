54a5cbdaea4e58790250017f6e18b16c2daad8b6
Radeonxz <xuanxxzhao@gmail.com>
2020-03-15 10:42:40 -0400

    chore: add reducer and redux dev tools extension

--- tipTax/src/store/index.ts
+++ tipTax/src/store/index.ts
@@ -1,3 +1,20 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
+import { rootReducer } from "./reducer";
 
-export const store = createStore(() => {});
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: Function;
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+export interface StoreState {
+  bill: number;
+  percentage: number;
+  split: number;
+}
+
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
