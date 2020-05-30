cf7685290648e7b856c68beb191e549bd62e309a
Radeonxz <xuanxxzhao@gmail.com>
2020-05-30 15:26:49 -0400

    chore: add incomeExpenses

--- expTracker/src/App.tsx
+++ expTracker/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
       </div>
     </>
   );
