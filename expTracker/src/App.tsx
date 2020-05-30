7141138b9bc134d812ebba14d880761e7f375f4a
Radeonxz <xuanxxzhao@gmail.com>
2020-05-30 15:22:07 -0400

    chore: add balance

--- expTracker/src/App.tsx
+++ expTracker/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "./components/Header";
+import Balance from "./components/Balance";
 
 import "./App.css";
 
@@ -8,7 +9,9 @@ const App = () => {
   return (
     <>
       <Header />
-      <div>Hello World...</div>
+      <div className="container">
+        <Balance />
+      </div>
     </>
   );
 };
