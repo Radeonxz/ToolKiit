7c5f644b651c8454d4a5fd0eba506d4760abf174
Radeonxz <xuanxxzhao@gmail.com>
2020-06-22 23:38:25 -0400

    chore: add Transaction

---
+++ expTracker/src/components/Transaction/Transaction.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Props {
+  transaction: {
+    text: string;
+    amount: number;
+  };
+}
+
+const Transaction = ({ transaction }: Props) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  return (
+    <li className="minus">
+      {transaction.text}
+      <span>
+        {sign}${transaction.amount}
+      </span>
+      <button className="delete-btn">x</button>
+    </li>
+  );
+};
+
+export default Transaction;
