9e7a15fe76806e0d13b241eb6f9708ac32e8619d
Radeonxz <xuanxxzhao@gmail.com>
2020-10-12 23:25:14 -0400

    chore: add wrapper styles

--- quizzz/src/App.styles.ts
+++ quizzz/src/App.styles.ts
@@ -20,3 +20,36 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Catamaran', sans-serfi;
   }
 `;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-item: center;
+
+  > p {
+    color: #fff;
+  }
+
+  .score {
+    color: #fff;
+    font-size: 2rem;
+    margin: 0;
+  }
+
+  hi {
+    font-family: Fascinate Inline, Haettenschweiler, "Arial Narrow Bold",
+      sans-serfi;
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    background-size: 100%;
+    background-clip: text;
+    // --webkit-background-clip: text;
+    // --webkit-text-fill-color: transparent;
+    // --moz-background-clip: text;
+    // --moz-text-fill-color: transparent;
+    filter: drop-shadow(2px 2px #0085a3);
+    font-size: 70px;
+    font-weight: 400;
+    text-align: center;
+    margin: 20px;
+  }
+`;
