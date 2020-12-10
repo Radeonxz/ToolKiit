51743cffaf52f45b885740e9a3fcd68284498e82
Radeonxz <xuanxxzhao@gmail.com>
2020-12-09 21:22:18 -0500

    chore: add create form

--- adminDash/client/src/components/PostCreate.js
+++ adminDash/client/src/components/PostCreate.js
@@ -3,7 +3,11 @@ import { Create, SimpleForm, TextInput, DeleteInput } from 'react-admin';
 const PostCreate = (props) => {
   return (
     <Create title='Create a Post' {...props}>
-      
+      <SimpleForm>
+        <TextInput source='title' />
+        <TextInput multiline source='body' />
+        <TextInput label='Published' source='publishedAt' />
+      </SimpleForm>
     </Create>
   )
 }
