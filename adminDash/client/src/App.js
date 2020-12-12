8ed9b731a5cfd1bd6a247e1928f2d9d1be98a1a0
Radeonxz <xuanxxzhao@gmail.com>
2020-12-11 23:29:37 -0500

    chore: users wip

--- adminDash/client/src/App.js
+++ adminDash/client/src/App.js
@@ -3,6 +3,9 @@ import restProvider from 'ra-data-simple-rest';
 import PostList from './components/PostList';
 import PostCreate from './components/PostCreate';
 import PostEdit from './components/PostEdit';
+import UserList from './components/UserList';
+import UserCreate from './components/UserCreate';
+import UserEdit from './components/UserEdit';
 
 function App() {
   return <Admin dataProvider={restProvider('http://localhost:3000')}>
@@ -10,6 +13,12 @@ function App() {
       list={PostList}
       create={PostCreate}
       edit={PostEdit}
+    />
+
+    <Resource name='users'
+      list={UserList}
+      create={UserCreate}
+      edit={UserEdit}
     />
   </Admin>
 }
