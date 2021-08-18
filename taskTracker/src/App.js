741a752516ea7006f2fd4b9ac24d8634454eb874
Radeonxz <xuanxxzhao@gmail.com>
2021-08-18 18:01:16 -0400

    chore: add tasks and task

--- taskTracker/src/App.js
+++ taskTracker/src/App.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
+const tasksInit = [
+  { id: 1, text: "Foo", day: "Jan 1st at 15:00", reminder: true },
+  { id: 2, text: "Bar", day: "Feb 2nd at 12:00", reminder: false },
+  { id: 3, text: "Baz", day: "May 3rd at 11:00", reminder: true }
+];
 
 const App = () => {
+  const [tasks, setTasks] = useState(tasksInit);
+
   return (
     <div className="container">
-      <Header title="Header Task Tracker" />
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 };
--- taskTracker/src/components/Button.js
+++ taskTracker/src/components/Button.js
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types";
 
-const Button = ({ color, text }) => {
+const Button = ({ color, text, onClick }) => {
   return (
     <div>
-      <button className="btn" style={{ backgroundColor: color }}>
+      <button
+        className="btn"
+        style={{ backgroundColor: color }}
+        onClick={onClick}
+      >
         {text}
       </button>
     </div>
@@ -15,7 +19,9 @@ Button.defaultProps = {
 };
 
 Button.prototype = {
-  color: PropTypes.string
+  color: PropTypes.string,
+  text: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 export default Button;
