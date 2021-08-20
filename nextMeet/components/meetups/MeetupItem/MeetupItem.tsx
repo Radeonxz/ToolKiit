c9721cce0eca7a190ed23e50f2410246834fda5a
Radeonxz <xuanxxzhao@gmail.com>
2021-08-19 22:32:15 -0400

    chore: update css class

--- nextMeet/components/meetups/MeetupItem/MeetupItem.tsx
+++ nextMeet/components/meetups/MeetupItem/MeetupItem.tsx
@@ -1,18 +1,20 @@
+import Image from "next/image";
+
 import Card from "../../ui/Card";
-import "./styles.css";
+import classes from "./styles.css";
 
 function MeetupItem(props: any) {
   return (
-    <li className="item">
+    <li className={classes.item}>
       <Card>
-        <div className="image">
-          <img src={props.image} alt={props.title} />
+        <div className={classes.image}>
+          <Image src={props.image} alt={props.title} />
         </div>
-        <div className="content">
+        <div className={classes.content}>
           <h3>{props.title}</h3>
           <address>{props.address}</address>
         </div>
-        <div className="actions">
+        <div className={classes.actions}>
           <button>Show Details</button>
         </div>
       </Card>
