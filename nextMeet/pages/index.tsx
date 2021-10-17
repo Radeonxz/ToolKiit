1e002bdaf302eb7cf397f24895d28ce2560aa576
RadeonXz <xuanxxzhao@gmail.com>
2021-10-16 20:24:38 -0400

    fix: update image config

--- nextMeet/pages/index.tsx
+++ nextMeet/pages/index.tsx
@@ -4,8 +4,7 @@ const meetupData = [
   {
     id: "m1",
     title: "First Meetup",
-    image:
-      "https://www.google.ca/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fb%2Fb6%2FImage_created_with_a_mobile_phone.png%2F1200px-Image_created_with_a_mobile_phone.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FImage&tbnid=gxFxsvFBmxeZ9M&vet=12ahUKEwjYuY6e1bvyAhUZqHIEHXDJCj8QMygAegUIARDNAQ..i&docid=0JWe7yDOKrVFAM&w=1200&h=900&q=image&ved=2ahUKEwjYuY6e1bvyAhUZqHIEHXDJCj8QMygAegUIARDNAQ",
+    image: "https://miro.medium.com/max/1838/1*2tmzU7bve-VlTkOMWsk_Hw.jpeg",
     address: "1 Foo, Bar",
     description: "This is the first one"
   }
